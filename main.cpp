32e1814e8e7e68ebc66c59301362574d43cda82e
Tusharbecoding <tusharchopra02122002@gmail.com>
2022-11-03 09:56:42 +0530

    completed this basic blockchain model

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include<ctime>
 #include<vector>
+#include<iostream>
 #include<string>
 using namespace std;
 
@@ -32,9 +33,128 @@ class Block{
         bool isHashValid();
 };  
 
+Block::Block(int idx, TransactionData d, size_t prevHash)
+{
+    index = idx;
+    data = d;
+    previousHash = prevHash;
+    blockHash = generateHash();
+}
+
+size_t Block::generateHash()
+{
+    hash<string> hash1;
+    hash<size_t> hash2;
+    hash<size_t> finalHash;
+
+    string toHash = to_string(data.amount) + data.receiverKey + data.senderKey + to_string(data.timestamp);
+
+    return finalHash(hash1(toHash) + hash2(previousHash));
+};
+
+size_t Block::getHash()
+{
+    return blockHash;
+}
+
+size_t Block::getPreviousHash()
+{
+    return previousHash;
+}
+
+bool Block::isHashValid()
+{
+    return generateHash() == blockHash;
+}
+
 // Blockchain
 class Blockchain{
     private: 
         Block createGenesisBlock(); //Block 0
     public: 
+        //public chain
+        vector<Block> chain;
+        //constructors
+        Blockchain();
+        //public functions
+        void addBlock(TransactionData data);
+        bool isChainValid();
+
+        //example
+        Block *getLatestBlock();
 };
+
+Blockchain::Blockchain()
+{
+    Block genesis = createGenesisBlock();
+    chain.push_back(genesis);
+}
+
+Block Blockchain::createGenesisBlock()
+{
+    time_t current;
+    TransactionData d;
+    d.amount = 0;
+    d.receiverKey = "None";
+    d.senderKey = "None";
+    d.timestamp = time(&current);
+
+    hash<int> hash1;
+    Block genesis(0, d, hash1(0));
+    return genesis;
+}
+
+//Demo only
+
+Block *Blockchain::getLatestBlock()
+{
+    return &chain.back();
+}
+
+void Blockchain::addBlock(TransactionData d)
+{
+    int index = (int)chain.size() - 1;
+    Block newBlock(index, d, getLatestBlock()->getHash());
+}
+
+bool Blockchain::isChainValid()
+{
+    vector<Block>::iterator it;
+    int chainLen = (int)chain.size();
+
+    for ( it = chain.begin(); it != chain.end(); ++it)
+    {
+        Block currentBlock = *it;
+        if (!currentBlock.isHashValid())
+        {
+            return false;
+        }
+        
+        if (chainLen > 1)
+        {
+            Block previousBlock = *(it - 1);
+            if (currentBlock.getPreviousHash() != previousBlock.getHash())
+            {
+                return false;
+            }
+            
+        }
+        
+    }
+    return true;
+}
+
+int main(){
+    Blockchain AwesomeCoin;
+
+    TransactionData data1;
+    time_t data1Time;
+    data1.amount = 1.5;
+    data1.receiverKey = "Joe";
+    data1.senderKey = "Sally";
+    data1.timestamp = time(&data1Time);
+
+    AwesomeCoin.addBlock(data1);
+
+    cout<<"Is chain valid?" <<endl << AwesomeCoin.isChainValid() <<endl;
+}
